5dcb4baa8ca0dfb449db5191bba72ba029be158c
Connor Maclachlan <connormac000@gmail.com>
2023-01-21 17:05:26 -0800

    Commited GUI file to main branch

---
+++ GUI.py
@@ -0,0 +1,10 @@
+"""
+Description: 
+For UO's CS 422 Project 1. A graphical user interface to interact with the data
+in the EasyA project. Allows input from students, system admins, and displays 
+data in bar graph form.
+"""
+
+import tkinter as tk
+import matplotlib as mpl
+
