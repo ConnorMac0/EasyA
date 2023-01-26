f9d60acd531b3aa76ec18117e8ae51ea844fa753
Connor Maclachlan <connormac000@gmail.com>
2023-01-26 11:11:00 -0800

    Initial landing page and user selection

--- GUI.py
+++ GUI.py
@@ -6,4 +6,54 @@ data in bar graph form.
 """
 
 import tkinter as tk
-import matplotlib as mpl
+
+#--------------GOALS-------------
+#Finish user and admin layouts
+#Determine how we want to accept inputs
+#Change so multiple windows are opened, change main window after user selection
+
+class GUI:
+    #initialize the landing page
+    def __init__(self):
+        self.root = tk.Tk()             
+        self.root.geometry("400x400")   #Set window size
+        self.root.title("EasyA")        #Give title
+
+        #Itroduction header of landing page
+        self.headlabel1 = tk.Label(self.root, text="Hello! And Weclome To The EasyA Program", font=('Arial', 18))
+        self.headlabel1.pack(padx=10, pady=10)
+
+        #Itroduction header of landing page
+        self.headlabel2 = tk.Label(self.root, text="Please Select Whether You Are A:", font=('Arial', 16))
+        self.headlabel2.pack(padx=10, pady=10)
+
+        #Button to select the student user and open student user page
+        self.stuBtn = tk.Button(self.root, text="Student", font=('Arial', 18), command=self.userPage)
+        self.stuBtn.pack(padx=10, pady=10)
+
+        #Button to select the system admin and open system admin page
+        self.sysABtn = tk.Button(self.root, text="System Admin", font=('Arial', 18), command=self.adminPage)
+        self.sysABtn.pack(padx=10, pady=10)
+
+        #GUI main loop
+        self.root.mainloop()
+
+    #Function that opens user page
+    def userPage(self):
+        
+        UserPage = tk.Toplevel()
+        UserPage.geometry("400x400")
+        
+        headlabel1 = tk.Label(UserPage, text="User Page", font=('Arial', 18))
+        headlabel1.pack(padx=10, pady=10)
+
+    #Function that opens system admin page
+    def adminPage(self):
+        
+        SysAdminPage = tk.Toplevel()
+        SysAdminPage.geometry("400x400")
+        
+        headlabel1 = tk.Label(SysAdminPage, text="System Admin Page", font=('Arial', 18))
+        headlabel1.pack(padx=10, pady=10)
+
+GUI()
