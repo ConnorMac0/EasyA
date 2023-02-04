6365d56b1c540ecfbcb42f6c92fa783748660348
Connor Maclachlan <connormac000@gmail.com>
2023-02-03 18:50:10 -0800

    Concated class dept codes and class numbers

--- GUI.py
+++ GUI.py
@@ -3,11 +3,17 @@ Description:
 For UO's CS 422 Project 1. A graphical user interface to interact with the data
 in the EasyA project. Allows input from students, system admins, and displays 
 data in bar graph form.
+Author: Connor Maclachlan
+Group: Group #4
+Created: 01/25/2023
+Last Updated: 
 """
 
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter import messagebox
+import json
 
 class GUI:
     # Initialize the landing page
@@ -18,14 +24,14 @@ class GUI:
         # New data file
         self.fp = None
 
-        # Natural Science department options
+        # Natural Science department names
         self.department = ["Anthropology", "Biology", "Chemistry and Biochemistry", "Computer Science", "Data Science", 
                         "Earth Sciences", "Geography", "Human Physiology", "Mathematics", "Neuroscience", "Physics", "Psychology"]
         # Department codes
-        self.dept_code = ["ANTH", "BI", "CH", "CIS", "DSC", "GEOL", "GEOG", "HPHY", "MATH", "NEUR", "PHYS", "PSY"]
+        self.dept_code = ["ANTH", "BI", "CH", "CIS", "DSCI", "GEOL", "GEOG", "HPHY", "MATH", "NEUR", "PHYS", "PSY"]
 
         # Course level options
-        self.course_levels = ["All 100-Level", "All 200-Level", "All 300-Level", "All 400-Level", "All 500-Level", "All 600-Level"]
+        self.course_levels = ["All Department", "All 100-Level", "All 200-Level", "All 300-Level", "All 400-Level", "All 500-Level", "All 600-Level"]
         self.specific_course = tk.StringVar()
 
         # Department and course level variables
@@ -35,23 +41,11 @@ class GUI:
         # Graph mode radio button variable
         self.mode = tk.IntVar()
         self.fac_type = tk.IntVar()
+        self.class_count = tk.IntVar()
 
         # Itroduction header of landing page
         self.headlabel1 = tk.Label(self.root, text="Hello! And Weclome To The EasyA Program", font=('Arial', 20))
         self.headlabel1.pack(padx=10, pady=10)
-
-        # Header explaining where data came from, how its limited, and the years included
-        self.headlabel2 = tk.Label(self.root, text="The data included in this program was initially copied from https://emeraldmediagroup.github.io/grade-data/," + 
-        " the data was copied on 01/15/2023, it includes class data from 2013-2016", font=('Arial', 10))
-        self.headlabel2.pack(padx=10, pady=10)
-
-        # Header explaining where data came from, how its limited, and the years included
-        self.headlabel3 = tk.Label(self.root, text='(ATTENTION) This data is limited, not all courses are included "If your class doesnt show up here, it means the data was redacted"', font=('Arial', 10))
-        self.headlabel3.pack(padx=10, pady=10)
-
-        # Header explaining where data came from, how its limited, and the years included
-        self.headlabel3 = tk.Label(self.root, text='- cited from the landing page of https://emeraldmediagroup.github.io/grade-data/', font=('Arial', 10))
-        self.headlabel3.pack(padx=10, pady=10)
 
         # Itroduction header of landing page
         self.headlabel4 = tk.Label(self.root, text="To Begin, Please Select Whether You Are A:", font=('Arial', 18))
@@ -64,6 +58,19 @@ class GUI:
         # Button to select the system admin and open system admin page
         self.sysABtn = tk.Button(self.root, text="System Admin", font=('Arial', 18), command=self.adminPage)
         self.sysABtn.pack(padx=10, pady=10)
+    
+        # Header explaining where data came from, how its limited, and the years included
+        self.cite_label1 = tk.Label(self.root, text="The data included in this program was copied from https://emeraldmediagroup.github.io/grade-data/," + 
+        " the data was initially copied on 01/15/2023, it includes class data from 2013-2016", font=('Arial', 8))
+        self.cite_label1.pack(padx=10, pady=10)
+
+        # Header explaining where data came from, how its limited, and the years included
+        self.cite_label2 = tk.Label(self.root, text='(ATTENTION) This data is limited, not all courses are included "If your class doesnt show up here, it means the data was redacted"', font=('Arial', 8))
+        self.cite_label2.pack(padx=10, pady=10)
+
+        # Header explaining where data came from, how its limited, and the years included
+        self.cite_label3 = tk.Label(self.root, text='- cited from the landing page of https://emeraldmediagroup.github.io/grade-data/', font=('Arial', 8))
+        self.cite_label3.pack(padx=10, pady=10)
 
         # GUI main loop
         self.root.mainloop()
@@ -73,29 +80,33 @@ class GUI:
 
         # Error checking for missing department
         if self.select_dept.get() == '':
-            print("ERROR: Please select a department")
+            messagebox.showerror('Error', 'Please Select a Department')
             return 0
         
         # Error checking for course level or specific course number
         if (self.select_level.get() == '') and (self.specific_course.get() == ''):
-            print("ERROR: Please select a cource level or specific course number")
+            messagebox.showerror('Error', 'Please Select a Couse Level or Specific Course Number')
             return 0
         
         # Turning the selected department name into department code
         dept_code = self.department.index(self.select_dept.get())
-        print(self.dept_code[dept_code])
+        #print(self.dept_code[dept_code])
 
         # Use course levels if no specific course number is given
         if self.specific_course.get() == '':
-            print(self.course_levels.index(self.select_level.get())+1)
+            if self.course_levels.index(self.select_level.get()) != 0:
+                print(self.dept_code[dept_code] + str(self.course_levels.index(self.select_level.get())))
+            else:
+                print(self.dept_code[dept_code])
 
         # Using specific course level only if it is not blank
         if self.specific_course.get() != '':
-            print(self.specific_course.get())
+            print(self.dept_code[dept_code] + self.specific_course.get())
         
         # Mode and faculty type selection
         print(self.mode.get())
         print(self.fac_type.get())
+        print(self.class_count.get())
 
     # Function containing student page
     def studentPage(self):
@@ -186,6 +197,18 @@ class GUI:
         justPass_radio_btn = tk.Radiobutton(graph_filter_frame, text="Just Pass - D's and F's", font=('Arial', 12), variable=self.mode, value=1)
         justPass_radio_btn.grid(row=2, column=1)
 
+        # Instructor selection label
+        instructor_label = tk.Label(graph_filter_frame, text="Include Class Count", font=('Arial', 20))
+        instructor_label.grid(row=0, column=2)
+
+        # Class count checkbox
+        class_count_check = tk.Checkbutton(graph_filter_frame, text="Include Class Count", font=('Arial', 12))
+        class_count_check.grid(row=1, column=2)
+
+        # Class count checkbox
+        class_count_check = tk.Checkbutton(graph_filter_frame, text="Include Class Count", font=('Arial', 12), variable=self.class_count)
+        class_count_check.grid(row=1, column=2)
+
         # Give all widgets in this frame the same padding
         for widget in graph_filter_frame.winfo_children():
             widget.grid_configure(padx=20, pady=10)
@@ -202,9 +225,9 @@ class GUI:
 
     # Function that enters data from the uploaded csv file
     def enter_data(self):
-        if self.fp == None:
-            print("Error: No file uploaded")
-            return None
+        if self.fp == '':
+            messagebox.showerror('Error', 'Please Select A File To Upload')
+            return 0
         fp = open(self.fp, "r")
         print(fp.read())
         fp.close()
@@ -227,7 +250,7 @@ class GUI:
         instructions_intro.pack()
 
         # Upload instructions
-        upload_inst = tk.Label(admin_info_frame, text='- Click the "Upload" button to select the csv data file you want to add to the system', font=('Arial', 12))
+        upload_inst = tk.Label(admin_info_frame, text='- Click the "Upload" button to select the JSON data file you want to add to the system', font=('Arial', 12))
         upload_inst.pack()
 
         # enter instructions
@@ -241,7 +264,7 @@ class GUI:
         data_entry_frame.grid(row=1, column=0, sticky="news", padx=10, pady=10)
 
         # Upload Button Label
-        upload_label = tk.Label(data_entry_frame, text="Upload CSV File", font=('Arial', 20))
+        upload_label = tk.Label(data_entry_frame, text="Upload JSON File", font=('Arial', 20))
         upload_label.grid(row=0, column=0)
 
         # Upload Button
